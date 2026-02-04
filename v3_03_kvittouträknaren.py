e2098e2041d65b8da6985c3a9dad2b95fb04bb0f
Starship100 <jimmy.widlund@outlook.com>
2026-02-04 13:28:39 +0100

    Kvittouträknaren version 2

--- v3_03_kvittouträknaren.py
+++ v3_03_kvittouträknaren.py
@@ -2,14 +2,32 @@
 print("Välkommen till Kvittouträknaren! ")
 print("Avsluta genom att skriva 'quit' eller 'avsluta'.")
 
-summa_tal = 0
-antal_personer = float(input("Hur många personer är ni? "))
-user_input = input("Skriv in ett belopp: ")
+antal_personer = int(input("Hur många personer är ni? "))
+user_input = input("Skriv in ett belopp: ")  # Lagrar användarens inmatade belopp. Används igen i while loop nedan
 
-#while user_input != "q" and user_input != "quit" and user_input != "avsluta":
+summa_tal = 0
+
+# Loopa användares svar tills avslut
+# while user_input != "q" and user_input != "quit" and user_input != "avsluta":
 while user_input not in ["q", "quit", "avsluta"]:
     summa_tal += int(user_input)
     user_input = input("Skriv in ett belopp: ")
 
-summa_per_person = summa_tal / antal_personer
-print("Det blir " + str(summa_tal) + "kr totalt, alltså " + str(summa_per_person) + "kr per person. Välkommen åter!")
+# Användaren har avslutat "Skriv in ett belopp: "
+# Hur mycket dricks vill användaren lägga?
+dricks_input = input("Hur många procent dricks vill ni lägga? ")
+
+dricks_procent = 0
+
+if dricks_input == "":  # Om användare inte anger ngt värde = automatiskt 10% dricks
+    #dricks_input = summa_tal * 0.10
+    dricks_procent = 10
+else:
+    dricks_procent = float(dricks_input) # Lägg inmatad % i variabeln 'dricks_procent'
+
+dricks_belopp = summa_tal * (dricks_procent / 100)  #
+total_med_dricks = summa_tal + dricks_belopp  # Ny variabel lägger ihop summan av tal och dricks
+
+summa_per_person = total_med_dricks / antal_personer
+
+print("Det blir " + str(total_med_dricks) + "kr totalt, alltså " + str(summa_per_person) + "kr per person. Välkommen åter!")
