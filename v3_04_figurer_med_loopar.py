32d54a3dba89e4ef3ba3d79100280e8b8d8edcb8
Starship100 <jimmy.widlund@outlook.com>
2026-02-04 17:25:31 +0100

    Figurer med loopar

--- v3_04_figurer_med_loopar.py
+++ v3_04_figurer_med_loopar.py
@@ -1,8 +1,50 @@
-for y in range(1, 10):
+
+for y in range(1, 7):
     s = ""
-    for x in range(1, 20):
+    for x in range(1, 9):
+        if x == 1:
+            s += "#"
+        else:
+            s += "."
+    print(s)
+
+print("-"*30)
+
+for y in range(1, 7):
+    s = ""
+    for x in range(1, 9):
         if x == y:
             s += "#"
         else:
             s += "."
     print(s)
+
+print("-" * 30)
+
+for y in range(1, 7):
+    s = ""
+    for x in range(1, 9):
+        if x == 3:
+            s += "#"
+        elif x == 4:
+            s += "#"
+        elif x == 5:
+            s += "#"
+        else:
+            s += "."
+    print(s)
+
+print("-"*30)
+
+for y in range(1, 7):
+    s = ""
+    for x in range(1, 9):
+        if x == 3:
+            s += "#"
+        elif x == 4:
+            s += "#"
+        else:
+            s += "."
+    print(s)
+
+print("-"*30)
