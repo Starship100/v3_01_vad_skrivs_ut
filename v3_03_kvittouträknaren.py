6faf6eb32b10d9d6d746b4a2613a508834912a47
Starship100 <jimmy.widlund@outlook.com>
2026-02-04 10:30:55 +0100

    Kvittouträknaren version 2

--- v3_03_kvittouträknaren.py
+++ v3_03_kvittouträknaren.py
@@ -3,6 +3,7 @@ print("Välkommen till Kvittouträknaren! ")
 print("Avsluta genom att skriva 'quit' eller 'avsluta'.")
 
 summa_tal = 0
+antal_personer = float(input("Hur många personer är ni? "))
 user_input = input("Skriv in ett belopp: ")
 
 #while user_input != "q" and user_input != "quit" and user_input != "avsluta":
@@ -10,4 +11,5 @@ while user_input not in ["q", "quit", "avsluta"]:
     summa_tal += int(user_input)
     user_input = input("Skriv in ett belopp: ")
 
-print("Det blir " + str(summa_tal) + "kr totalt. Välkommen åter!")
+summa_per_person = summa_tal / antal_personer
+print("Det blir " + str(summa_tal) + "kr totalt, alltså " + str(summa_per_person) + "kr per person. Välkommen åter!")
