ad6e3eecd819ed6323918781c18372a498055055
Starship100 <jimmy.widlund@outlook.com>
2026-02-04 14:58:30 +0100

    Kvittouträknaren version 2

---
+++ Kvittouträknaren_komprimerad.py
@@ -0,0 +1,19 @@
+print("Välkommen till Kvittouträknaren! ")
+print("Avsluta genom att skriva 'quit' eller 'avsluta'.")
+antal_personer = int(input("Hur många personer är ni? "))
+user_input = input("Skriv in ett belopp: ")  # Lagrar användarens inmatade belopp. Används igen i while loop nedan
+summa_tal = 0
+while user_input not in ["q", "quit", "avsluta"]:
+    summa_tal += int(user_input)
+    user_input = input("Skriv in ett belopp: ")
+dricks_input = input("Hur många procent dricks vill ni lägga? ")
+dricks_procent = 0
+if dricks_input == "":  # Om användare inte anger ngt värde = automatiskt 10% dricks
+    #dricks_input = summa_tal * 0.10
+    dricks_procent = 10
+else:
+    dricks_procent = float(dricks_input) # Lägg inmatad % i variabeln 'dricks_procent'
+dricks_belopp = summa_tal * (dricks_procent / 100)  #
+total_med_dricks = summa_tal + dricks_belopp  # Ny variabel lägger ihop summan av tal och dricks
+summa_per_person = total_med_dricks / antal_personer
+print("Det blir " + str(total_med_dricks) + "kr totalt, alltså " + str(summa_per_person) + "kr per person. Välkommen åter!")
