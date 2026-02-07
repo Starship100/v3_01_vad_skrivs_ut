659117e091b256dff3bcec836fa149813522da44
Starship100 <jimmy.widlund@outlook.com>
2026-02-07 14:49:26 +0100

    Test

---
+++ v3_06_ToDO_list.py
@@ -0,0 +1,28 @@
+
+todo_list = []
+
+print("*** Todo List ***")
+
+while True:
+    print("1. Se innehållet i din lista")
+    print("2. Lägg till nya punkter till din lista")
+
+    choice = input("Välj ett alternativ: ")
+
+    if choice == "1":
+        if len(todo_list) == 0:
+            print("Listan är tom")
+        else:
+            for item in todo_list:
+                print(item)
+
+    elif choice == "2":
+        todo_list = input("Lägg till: ")
+
+
+
+
+# input = input("Lägg till saker i din Todo-list: ")
+# todo_list = input.split(",")
+
+#print(todo_list)
